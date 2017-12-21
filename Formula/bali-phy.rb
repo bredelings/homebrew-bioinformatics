526d8e30c5cd07c0bbe880642fe849b424d51672
Benjamin Redelings <benjamin.redelings@gmail.com>
2017-12-21 14:54:42 -0500

    First attempt at writing a Formula that works with meson.

--- Formula/bali-phy.rb
+++ Formula/bali-phy.rb
@@ -5,24 +5,16 @@ class BaliPhy < Formula
   stable do
     url "https://github.com/bredelings/BAli-Phy.git",
         :using => :git,
-        :revision => "44495906c32f6c12d5c36d81ea6be4ed86f2efae"
-    version "3.0-beta5+1"
+        :revision => "7c46f800d533947b34142fdcf9597f14335b8c37"
+    version "3.0-beta6+1"
   end
-
-#  bottle do
-#    sha256 "50fa703af91bb78303464e6e02db8603e8722e01a1275c8cc6b44254b9dacfab" => :sierra
-#    sha256 "62fa80287c762052da28393ff946765c1390c1da0c917cdbac6e98f0600856e9" => :el_capitan
-#    sha256 "9a243403ab53dc740dbfb5f34eb829fd4aa777bed544ece0d09c040994ad0b14" => :yosemite
-#    sha256 "86b5806848c469aaae1df3417ccfcba46468f251765235bb33036678a7176d15" => :x86_64_linux
-#  end
 
   head do
     url "https://github.com/bredelings/BAli-Phy.git", :using => :git
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
 
   depends_on "cairo"
@@ -30,17 +22,15 @@ class BaliPhy < Formula
   needs :cxx14
 
   def install
-    system "./bootstrap.sh"
-    mkdir "macbuild" do
-      system "../configure", "--disable-debug",
-                             "--disable-dependency-tracking",
-                             "--prefix=#{prefix}"
-      system "make", "install"
+    system 'meson', 'macbuild', "--prefix=#{prefix}"
+    cd 'macbuild' do
+      system 'ninja'
+      system 'ninja', 'install'
     end
   end
 
   test do
     system "#{bin}/bali-phy", "#{pkgshare}/examples/sequences/5S-rRNA/5d.fasta", "--iter=150"
-    system "#{bin}/bp-analyze.pl", "5d-1"
+    system "#{bin}/bp-analyze", "5d-1"
   end
 end
