b1f9b357dac938d8f02fa394810e31afee3fdc8a
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-05-25 00:37:34 +0100

    Update to bali-phy 4.0-beta11

--- Formula/bali-phy.rb
+++ Formula/bali-phy.rb
@@ -1,36 +1,48 @@
 class BaliPhy < Formula
-  desc "Simultaneous Bayesian estimation of alignment and phylogeny"
-  homepage "http://www.bali-phy.org/"
+  # cite Redelings_2014: "https://dx.doi.org/10.1093/molbev/msu174"
+  # cite Redelings_2021: "https://doi.org/10.1093/bioinformatics/btab129"
+  desc "Bayesian co-estimation of phylogenies and multiple alignments"
+  homepage "https://www.bali-phy.org/"
+  url "https://github.com/bredelings/BAli-Phy.git",
+    tag:      "4.0-beta11",
+    revision: "52255f775469844c82bbff2c69b63abbff54527e"
+  license "GPL-2.0-or-later"
+  head "https://github.com/bredelings/BAli-Phy.git", branch: "master"
 
-  stable do
-    url "https://github.com/bredelings/BAli-Phy.git",
-        :using => :git,
-        :revision => "20fc9bd890220b99f76d0a3d4e7b9190370bb3bd"
-    version "3.3"
+  livecheck do
+    url :stable
+    strategy :github_latest
   end
 
-  head do
-    url "https://github.com/bredelings/BAli-Phy.git", :using => :git
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 cellar: :any, arm64_sonoma: "5bbbb43e83128398483585a9107243a3b50b73fc6c97c57e033c8fa21f2a4984"
   end
 
+  depends_on "eigen" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
-  depends_on "pkg-config" => :build
   depends_on "pandoc" => :build
-
+  depends_on "pkg-config" => :build
+  depends_on "boost"
   depends_on "cairo"
+  depends_on "gcc" unless OS.mac? # for C++20
 
-  needs :cxx14
+  # C++20
+  fails_with gcc: "5"
+  fails_with gcc: "6"
+  fails_with gcc: "7"
+  fails_with gcc: "8"
+  fails_with gcc: "9"
 
   def install
-    system 'meson', 'macbuild', "--prefix=#{prefix}", '-Dextra-tools=False'
-    cd 'macbuild' do
-      system 'ninja', 'install'
-    end
+    flags = %w[-C build install]
+    system "meson", "build", "--prefix=#{prefix}", "--buildtype=release", "-Db_ndebug=true"
+    system "ninja", *flags
   end
 
   test do
-    system "#{bin}/bali-phy", "#{doc}/examples/sequences/5S-rRNA/5d.fasta", "--iter=150"
-    system "#{bin}/bp-analyze", "5d-1"
+    system "#{bin}/bali-phy", "--version"
+    system "#{bin}/bali-phy", "#{doc}/examples/5S-rRNA/5d.fasta", "--iter=150"
   end
 end
